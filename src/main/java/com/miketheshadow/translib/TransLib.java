966156d47318bc4d369a111bb60b2fd536a1c1a5
miketheshadow <miketheshadow1@gmail.com>
2022-04-01 15:12:52 -0600

    Added ability to directly get a translation from a specified config without needing to make a configmanager instance yourself

--- src/main/java/com/miketheshadow/translib/TransLib.java
+++ src/main/java/com/miketheshadow/translib/TransLib.java
@@ -12,9 +12,10 @@ public class TransLib {
 
     /**
      * The base config must contain all possible keys. Lacking all keys will cause severe stability issues
+     *
      * @param baseConfigName The config to use as a template for all of them
-     * @param configFolder The folder that contains your configurations
-     * @param activeConfig The currently active configuration
+     * @param configFolder   The folder that contains your configurations
+     * @param activeConfig   The currently active configuration
      */
     public TransLib(@Nonnull String baseConfigName, @Nonnull File configFolder, @Nonnull String activeConfig) {
         if (!configFolder.isDirectory()) {
@@ -26,8 +27,9 @@ public class TransLib {
 
     /**
      * Using this constructor assumes the primary configuration file is the base configuration file
+     *
      * @param baseConfigName The config to use as a template for all of them
-     * @param configFolder The folder that contains your configurations
+     * @param configFolder   The folder that contains your configurations
      */
     public TransLib(@Nonnull String baseConfigName, @Nonnull File configFolder) {
         if (!configFolder.isDirectory()) {
@@ -41,7 +43,8 @@ public class TransLib {
      * Use this to allow for user-specific translations. Using a global {@see ConfigManager}
      * This allows you to create a new TransLib instance per user which can be as simple as
      * PlayerMap#get#getTranslation to get translations per player rather than globally
-     * @param manager a pre-loaded manager
+     *
+     * @param manager      a pre-loaded manager
      * @param activeConfig The currently active configuration
      */
     public TransLib(@Nonnull ConfigManager manager, @Nonnull String activeConfig) {
@@ -54,14 +57,18 @@ public class TransLib {
      * @return The key pulled from the current active configuration
      */
     public String getTranslation(String key) {
-        return configManager.getValue(this.activeConfig, key);
+        return this.configManager.getValue(this.activeConfig, key);
     }
 
     public String getActiveConfig() {
-        return activeConfig;
+        return this.activeConfig;
     }
 
     public void setActiveConfig(String activeConfig) {
         this.activeConfig = activeConfig;
+    }
+
+    public String getTranslationFromConfig(String config, String key) {
+        return this.configManager.getValue(config, key);
     }
 }
